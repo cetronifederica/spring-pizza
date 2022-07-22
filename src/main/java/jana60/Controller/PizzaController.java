f51b7a711a8ed431ceb657f3af05309a307abfa2
federicacetroni <cetronifederica97@gmail.com>
2022-07-22 17:10:49 +0200

    aggiunti edit e delete

--- src/main/java/jana60/Controller/PizzaController.java
+++ src/main/java/jana60/Controller/PizzaController.java
@@ -1,10 +1,21 @@
 package jana60.Controller;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jana60.Model.Pizza;
 import jana60.Repository.PizzaRepository;
@@ -22,9 +33,61 @@ public class PizzaController {
 		return "/pizza/pizza";
 	}
 
-	@GetMapping("/pizza/add")
+	@GetMapping("/add")
 	public String pizzaForm(Model model) {
 		model.addAttribute("pizza", new Pizza());
 		return "/pizza/add";
+	}
+
+	@PostMapping("/save") // sezione di aggiunta della pizza al database
+	public String save(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult br, Model model) {
+		boolean hasErrors = br.hasErrors();
+
+		if (br.hasErrors()) {
+			return "/pizza/add";
+		}
+
+		if (hasErrors) {
+			// se ci sono errori non salvo la pizza su database ma ritorno alla form
+			// precaricata
+			return "/pizza/edit";
+		} else {
+			// se non ci sono errori salvo la pizza che arriva dalla form
+			try {
+				repo.save(formPizza);
+			} catch (Exception e) { // gestisco eventuali eccezioni sql
+				model.addAttribute("errorMessage", "Unable to save the Pizza");
+				return "/pizza/edit";
+			}
+			return "redirect:/";
+		}
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Integer pizzaId, Model model) {
+		Optional<Pizza> result = repo.findById(pizzaId);
+
+		if (result.isPresent()) {
+
+			model.addAttribute("pizza", result.get());
+			return "/pizza/add";
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La pizza con id " + pizzaId + " non è presente");
+		}
+
+	}
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Integer pizzaId, RedirectAttributes ra) {
+		Optional<Pizza> result = repo.findById(pizzaId);
+		if (result.isPresent()) {
+
+			repo.delete(result.get());
+			ra.addFlashAttribute("successMessage", "Pizza " + result.get().getName() + " deleted!");
+			return "redirect:/";
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La pizza con id " + pizzaId + " non è presente");
+		}
+
 	}
 }
--- src/main/java/jana60/Model/Pizza.java
+++ src/main/java/jana60/Model/Pizza.java
@@ -1,18 +1,74 @@
 package jana60.Model;
 
+import java.text.DecimalFormat;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Pizza {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Integer Id;
 
+	@NotEmpty(message = "Non esistono pizze senza nome")
+	@Column(nullable = false)
 	private String name;
+
+	@Lob
 	private String description;
+
+	@NotNull
+	@DecimalMin("0.00")
+	@DecimalMax("100.00")
 	private Double price;
+
+	// getter e setter
+	public Integer getId() {
+		return Id;
+	}
+
+	public void setId(Integer id) {
+		this.Id = Id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getFormattedPrice(Double price) {
+		DecimalFormat df = new DecimalFormat("##.00€");
+
+		return df.format(price);
+	}
+
 }
